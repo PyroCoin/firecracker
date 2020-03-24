a1d8ae680213fc629d3ac9f4f207bc3d597861f5
Tejraj Jadeja <tejrajjadeja@Tejrajs-MacBook-Pro.local>
2020-03-23 19:14:31 -0700

    Made the Class Blueprint

--- PyroBlockChain.py
+++ PyroBlockChain.py
@@ -7,3 +7,25 @@ from uuid import uuid4
 import requests
 from flask import Flask, jsonify, request
 
+class BlockChain(object):
+    def __init__(self):
+        self.chain = []
+        self.currentTransactions = []
+
+    def newBlock(self):
+        #Makes a new block
+        pass
+    def newTransaction(self):
+        #Creates a transaction
+        pass
+    
+    
+    @staticmethod
+    def hash(block):
+        #Function that hashes a block
+        pass
+
+    @property
+    def lastBlock(self):
+        #returns the last block, creating a blockchain
+        
