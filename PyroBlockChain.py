97fd72c6a351d37ae9825bf80c3d935848a51e5a
JetDeveloping <jetjadeja@gmail.com>
2020-03-23 22:47:01 -0700

    Added mining

--- PyroBlockChain.py
+++ PyroBlockChain.py
@@ -121,11 +121,37 @@ blockchain = BlockChain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return 'Mine a new block'
+    lastBlock = blockchain.lastBlock
+    lastProof = lastBlock['proof']
+    proof = blockchain.proof_of_work(lastProof)
+
+    blockchain.newTransaction(
+        sender='0'
+        recipient=nodeIdentifier
+        amount=7
+    )
+
+    response = {
+        'message': 'New Block Made',
+        'index': block['index'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
 
 @app.route('/transactions/new', methods=['POST'])
 def newTransaction():
-    return 'We will add a new transaction'
+    values = request.get_json()
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    index = blockchain.newTransaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+
 
 @app.route('/chain', methods=['GET'])
 def fullChain():
