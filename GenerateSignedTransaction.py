3e18e1e020059d6722eabda04fb6d0ef78205132
Transmissions <greenfeatherhelp@gmail.com>
2020-03-26 14:37:38 -0700

    Use private keys for sender & public for recipient

--- GenerateSignedTransaction.py
+++ GenerateSignedTransaction.py
@@ -3,13 +3,12 @@ from json import dumps
 from ecdsa import SigningKey
 import json
 
-
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-s', '--sender', default="ignore", type=str, help='sender')
-    parser.add_argument('-r', '--recipient', default="ignore", type=str, help='recipient')
+    parser.add_argument('-s', '--sender', default="ignore", type=str, help="sender private key")
+    parser.add_argument('-r', '--recipient', default="ignore", type=str, help='recipient public key')
     parser.add_argument('-a', '--amount', default=200, type=int, help='amount to send')
 
     args = parser.parse_args()
@@ -22,16 +21,11 @@ if __name__ == '__main__':
         s_pub_key = sender.get_verifying_key().to_string().hex()
 
     if args.recipient != "ignore":
-        recipient = SigningKey.from_string(bytes.fromhex(args.recipient))
-        r_pub_key = recipient.get_verifying_key().to_string().hex()
+        r_pub_key = recipient = args.recipient
     else:
         recipient = SigningKey.generate()
         r_pub_key = recipient.get_verifying_key().to_string().hex()
 
-
-    transaction = {'sender': s_pub_key, 'recipient': r_pub_key, 'amount': args.amount}
-    transaction['signature'] = sender.sign(f"{s_pub_key} -{args.amount}-> {r_pub_key}".encode()).hex()
+    transaction = {'sender': s_pub_key, 'recipient': r_pub_key, 'amount': args.amount,
+                   'signature': sender.sign(f"{s_pub_key} -{args.amount}-> {r_pub_key}".encode()).hex()}
     print(json.dumps(transaction))
-
-
-
